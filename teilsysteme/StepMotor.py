18f2afd172f1e5695f13349ac1b1a119e80fffff
Jordi Buscaglia <Jordi.Buscaglia577@gmail.com>
2023-12-17 23:02:09 +0100

    # Please enter the commit message for your changes. Lines starting

--- teilsysteme/StepMotor.py
+++ teilsysteme/StepMotor.py
@@ -2,10 +2,11 @@ import HBridge as hb
 import UltraSonic as us
 
 class StepMotor():
-    hbridge = hb.HBridge()
-    self.circumference = 0.05
-    self.stepsize = 1.8  
-    self.frequency = 10
+    def __init__(self, pin_pul, pin_dir, pin_enable):
+        hbridge = hb.HBridge(pin_pul, pin_dir, pin_enable)
+        self.circumference = 0.05
+        self.stepsize = 1.8  
+        self.frequency = 10
 
     def step_per_revolution(self) -> int: 
         return 360 / self.stepsize
@@ -13,16 +14,16 @@ class StepMotor():
     def amount_of_steps(self, distance:int):
         steps = self.steps_per_revolution() * distance / self.circumference 
 
-    def set_speed_x(speed: int):  
+    def set_speed_x(self, speed: int):  
         self.frequency = speed/60
 
-    def set_speed_y(speed: int):  
+    def set_speed_y(self, speed: int):  
         self.frequency = speed/60
 
-    def set_direction_x(dir: bool):
+    def set_direction_x(self, dir: bool):
         self.hbridge.set_x_direction(True)
 
-    def set_direction_x(dir: bool):
+    def set_direction_y(self, dir: bool):
         self.hbridge.set_y_direction(True)
 
     def move_x(self, distance: int): 
