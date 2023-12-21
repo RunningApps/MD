5296a83dfea1ece132cce686a56c361adf4d0d4d
Jordi Buscaglia <Jordi.Buscaglia577@gmail.com>
2023-12-21 17:35:02 +0100

    Überarbeitung des importiersystems

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from teilsysteme import Verfahrwege as vw
-from teilsysteme import StepMotor as sm
-import SimulRPi.GPIO as GPIO
+from modules import Verfahrwege
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    import SimulRPi.GPIO as GPIO
 
 if __name__ == '__main__':
-    #x_stepmotor = sm.StepMotor(2, 4, 6)
-    #y_stepmotor = sm.StepMotor(8, 10, 12)
+    #x_stepmotor = StepMotor(2, 4, 6)
+    #y_stepmotor = StepMotor(8, 10, 12)
    
     #x_stepmotor.move(0.02, "Right", 1000)
     #y_stepmotor.move(0.02, "Left", 1000)
 
     #print("Exiting main process...")
     #GPIO.cleanup()
-    
-    vw = vw.Verfahrwege()
+   
+    vw = Verfahrwege()
     vw.route_1()
     
     #GPIO.cleanup()
--- teilsysteme/test.py
+++
@@ -1,8 +0,0 @@
-def foo():
-    return 5
-
-def test_func(my_eval_string):
-    res = eval(my_eval_string)
-    return res
-
-print(test_func("foo()"))
