7652f4ae2e3cddfb852c6729c206d6f8905fbcff
Jordi <Jordi.buscaglia577@gmail.com>
2024-01-09 15:34:35 +0100

    09012024

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from modules import Verfahrwege
+import modules as module
 import SimulRPi.GPIO as GPIO
 
 if __name__ == '__main__':
-    #x_stepmotor = StepMotor(2, 4, 6)
+    x_stepmotor = module.StepMotor(21, 20, 16)
     #y_stepmotor = StepMotor(8, 10, 12)
    
-    #x_stepmotor.move(0.02, "Right", 1000)
+    x_stepmotor.move(0.02, "Right", 1000)
     #y_stepmotor.move(0.02, "Left", 1000)
 
     #print("Exiting main process...")
     #GPIO.cleanup()
    
-    vw = Verfahrwege()
-    vw.route_1()
+    #vw = Verfahrwege()
+    #vw.route_1()
     
     #GPIO.cleanup()
     #data_request_event = "Marke1;Gummi;300;30;Marke2;Hartgummi;500;50;100;Staub;1;123.5"
